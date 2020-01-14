eeeaa74e78d76bbe53b3efc9af2088244f19959e
Andrew Graf <38439152+humeinstein@users.noreply.github.com>
2020-01-14 16:42:12 -0500

    Delete printCoinsRequests.py

--- coinPaprika/printCoinsRequests.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" print coins using requests """
-import requests
-import sys
-
-if __name__ == '__main__':
-
-    json = requests.get("https://api.coinpaprika.com/v1/coins").json()
-    print (json)
